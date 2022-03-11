fbd0f75c901256f4684d9cf0c7d3bd479d369e5d
immaturehearts <1120841716@qq.com>
2022-03-11 16:39:32 +0800

    Initial commit

---
+++ src/test/java/com/epcm/EpcmServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epcm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpcmServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
